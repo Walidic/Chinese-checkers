940ca9bc54056bc145e5aa8938f5fec66f79ec69
Walidic <74151292+Walidic@users.noreply.github.com>
2022-06-04 23:37:20 +0200

    fixees

--- Game/constants.py
+++ Game/constants.py
@@ -1,30 +1,18 @@
 import pygame
 
-
 WIDTH, HEIGHT = 625, 425
-
 ROWS, COLS = 17, 25
-
 SQUARE_SIZE = WIDTH // COLS
 
-
 # BOARD COLORS
-
 RED = (255, 0, 0)
-
 GREEN = (0, 183, 0)
-
 BLUE = (21, 105, 199)
-
 WHITE = (200, 157, 124)
-
 GREY = (255, 255, 255)
-
 YELLOW = (255, 216, 1)
 
-
 # PIECE COLORS
-
 BLACK = (0, 0, 0)
-
+PIECE_GREEN = (152, 255, 152)
 PIECE_BLUE = (130, 202, 250)
